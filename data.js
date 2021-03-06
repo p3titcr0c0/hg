13a418b3da8188d3a11a27b9a49392ea0249ebc0
petitcroco <58482685+p3titcr0c0@users.noreply.github.com>
2021-03-06 17:11:55 +0100

    Create data.js

---
+++ data.js
@@ -0,0 +1,16 @@
+const online = [
+    {
+      "name": "Molecule Man",
+      "age": 29,
+      "secretIdentity": "Dan Jukes",
+      "powers": [
+        "Radiation resistance",
+        "Turning tiny",
+        "Radiation blast"
+      ]
+    }
+  ]
+/*
+https://cdn.jsdelivr.net/gh/p3titcr0c0/hg@main/online.js
+https://www.jsdelivr.com/rawgit
+*/
