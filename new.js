05fbda48e5a2eeaa5cbf4c177d9f6192ea64ed2d
petitcroco <58482685+p3titcr0c0@users.noreply.github.com>
2021-03-06 17:47:32 +0100

    Create new.js

---
+++ new.js
@@ -0,0 +1,26 @@
+const online = [
+    {
+      "logo": "4.png",
+      "titre": "Course à vélo",
+      "position": "Beaumont-Hague",
+      "positionPlus": "En face du super U",
+      "date": "11/03/2021 00:01",
+      "informations": "CCH",
+    },
+    {
+      "logo": "23.png",
+      "titre": "Boire un soda",
+      "position": "Buvette",
+      "positionPlus": "A côté de la piscine",
+      "date": "12/03/2021 00:01",
+      "informations": "Coca-cola",
+    },
+    {
+      "logo": "15.png",
+      "titre": "S'occuper d'un chien",
+      "position": "Animalerie",
+      "positionPlus": "Zone commerciale",
+      "date": "13/03/2021 00:01",
+      "informations": "Animalerie 06.81....",
+    },
+]
